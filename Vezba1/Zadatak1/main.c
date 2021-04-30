01d23edaf603b531aae81e77ee16b36d640f1569
Vasilije <vasilijebatas@gmail.com>
2021-04-30 10:16:41 +0200

    PrintResult function added

--- Vezba1/Zadatak1/main.c
+++ Vezba1/Zadatak1/main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+
+void PrintResult(int res)
+{
+printf("Result: %d\n", res);
+}
+
+
 int main()
 {
 int a = 3;
 int b = 8;
 int c;
+
 if(a > b)
-c = a - b;
+	c = a - b;
 else
-c = b - a;
-printf("Result: %d\n", c);
+	c = b - a;
+
+PrintResult(c);
+
 return 0;
 }
